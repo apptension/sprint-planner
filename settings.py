8d775d90f3735fbd1fe122bb0a79badbf329e2d0
Mateusz Wójcik <mateusz.wojcik@apptension.com>
2021-12-05 11:29:04 +0100

    Change default JQL to get currentUser assigned issues

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ JIRA_USER = os.getenv('JIRA_USER')
 JIRA_TOKEN = os.getenv('JIRA_TOKEN')
 JIRA_PROJECT = os.getenv('JIRA_PROJECT')
 JIRA_ESTIMATE_FIELD = os.getenv("JIRA_ESTIMATE_FIELD", "timeoriginalestimate")
-JIRA_ISSUES_JQL = os.getenv("JIRA_ISSUES_JQL", 'project=' + JIRA_PROJECT + " and sprint in openSprints()")
+JIRA_ISSUES_JQL = os.getenv("JIRA_ISSUES_JQL", 'project=' + JIRA_PROJECT + " and sprint in openSprints() AND assignee = currentUser()")
 JIRA_PRIORITY_ORDER = os.getenv("JIRA_PRIORITY_ORDER", "ASC")
 
 # GOOGLE
